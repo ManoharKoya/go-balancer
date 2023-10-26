09486e650d0ce22129700e602cd8cc50861d6d5f
Manohar Koya <manoharkoya@Manohars-MacBook-Air.local>
2023-10-26 04:03:25 -0500

    go-balancer/issues/2: added logic for health checking backend servers

--- internal/lbcore/algorithms/roundrobin.go
+++ internal/lbcore/algorithms/roundrobin.go
@@ -1,1 +1,15 @@
 package algorithms
+
+import "go-balancer/internal/lbcore/servers"
+
+type RoundRobinLoadBalancer struct {
+	servers []servers.BackendServer
+}
+
+func New(servers []servers.BackendServer) *RoundRobinLoadBalancer {
+	return &RoundRobinLoadBalancer{servers: servers}
+}
+
+func (lb *RoundRobinLoadBalancer) SelectServer() *servers.BackendServer {
+	return &lb.servers[0]
+}
